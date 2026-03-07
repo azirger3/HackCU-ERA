738d4da09b58034bce73cdac60749165109cf288
Edward Wawrzynek <edward@wawrzynek.com>
2026-03-07 15:48:47 -0700

    add basic flow graph

---
+++ src/renderer/App.jsx
@@ -0,0 +1,64 @@
+import { useCallback, useState } from 'react';
+import {
+  ReactFlow,
+  addEdge,
+  applyEdgeChanges,
+  applyNodeChanges,
+} from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+import './Styles/index.css';
+ 
+import TextUpdaterNode from './components/TextUpdaterNode';
+ 
+const rfStyle = {
+  backgroundColor: '#B8CEFF',
+};
+ 
+const initialNodes = [
+  {
+    id: 'node-1',
+    type: 'textUpdater',
+    position: { x: 100, y: 0 },
+    data: { value: 123 },
+  },
+  {
+    id: 'node-2',
+    type: 'output',
+    targetPosition: 'top',
+    position: { x: 0, y: 200 },
+    data: { label: 'node 2' },
+  },
+  {
+    id: 'node-3',
+    type: 'output',
+    targetPosition: 'top',
+    position: { x: 200, y: 200 },
+    data: { label: 'node 3' },
+  },
+];
+ 
+const initialEdges = [
+  { id: 'edge-1', source: 'node-1', target: 'node-2', sourceHandle: 'a' },
+  { id: 'edge-2', source: 'node-1', target: 'node-3', sourceHandle: 'b' },
+];
+ 
+// we define the nodeTypes outside of the component to prevent re-renderings
+// you could also use useMemo inside the component
+const nodeTypes = { textUpdater: TextUpdaterNode };
+ 
+function Flow() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+ 
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      nodeTypes={nodeTypes}
+      fitView
+      style={rfStyle}
+    />
+  );
+}
+ 
+export default Flow;
---
+++ src/renderer/components/TextUpdaterNode.jsx
@@ -0,0 +1,16 @@
+import { Handle, Position } from '@xyflow/react';
+ 
+const handleStyle = { left: 10 };
+ 
+function TextUpdaterNode(props) {
+  return (
+    <div className="text-updater-node">
+      <div>Custom Node</div>
+      <Handle type="target" position={Position.Top} />
+      <Handle type="source" position={Position.Bottom} id="a" style={handleStyle} />
+      <Handle type="source" position={Position.Bottom} id="b" />
+    </div>
+  );
+}
+ 
+export default TextUpdaterNode;
